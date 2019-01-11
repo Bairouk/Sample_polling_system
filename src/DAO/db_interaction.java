e95d521386340bdd95a3c18d3253dd1258f52b68
Anass Bairouk <anass.bairouk@gmail.com>
2019-01-11 14:51:40 +0100

    login and some func

--- src/DAO/db_interaction.java
+++ src/DAO/db_interaction.java
@@ -3,42 +3,34 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
+
+//test is at the bottom
+
 public class db_interaction {
 
-    static String url="jdbc:mysql://localhost:8889/poll";
-    static Connection conn;
-    static Statement st;
-
-    public static Connection connection(){
-
+    static String url="jdbc:mysql://localhost:3306/poll";
+    private static Connection _conn;
+    static {
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //etablir la connection
-            conn = DriverManager.getConnection(url,"root","");
-
+            _conn = DriverManager.getConnection(url,"root","");
         } catch (ClassNotFoundException | SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return conn;
+    }
 
+    public static Connection _get_connection(){
+        return _conn;
     }
 
 
-    public static void connect()
-    {
-        //chargement du pilot
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            //etablir la connection
-            conn = DriverManager.getConnection(url,"root","");
-            //preparation d instruction
-            st = conn.createStatement();
 
-        } catch (ClassNotFoundException | SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    //This just a test
+    public static void main(String[] args) {
+        Connection _connection = db_interaction._get_connection();
+        System.out.println("OKAY IT WORKS ,HAPPY NOW --> "+_connection.toString());
     }
 }
+
+
+
---
+++ src/web/_login_servlet.java
@@ -0,0 +1,10 @@
+package web;
+
+import javax.servlet.http.HttpServlet;
+
+public class _login_servlet extends HttpServlet {
+
+
+
+
+}
